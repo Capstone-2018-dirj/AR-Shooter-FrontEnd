fa7ca239ac584f3d36e483769252a2b02861a429
richardsunny2015 <richardsunny2015@gmail.com>
2018-07-05 12:41:59 -0400

    chore(socket.js): removed /

--- socket.js
+++ socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-export const host = 'https://ar-shooter-server.herokuapp.com/';
+export const host = 'https://ar-shooter-server.herokuapp.com';
 const socket = io(host);
 export default socket;
